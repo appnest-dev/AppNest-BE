4034643e99bf063ff521d60b1442c1b61e65ca50
Mazen Adel <ma134652134652@gmail.com>
2024-05-31 16:08:28 +0300

    refactor(healthchecker.route.ts): add projects route health check

--- src/routes/healthChecker.route.ts
+++ src/routes/healthChecker.route.ts
@@ -1,19 +1,57 @@
-import { NextFunction, Request, Response, Router } from "express";
-import asyncWrapper from "../middlewares/asyncWrapper";
+import express from "express";
 import sequelize from "../config/db";
-import httpStatusText from "../utils/httpStatusText";
+import "dotenv/config";
 
-const router = Router();
+const BASE_URL = `http://localhost:${process.env.PORT}/api`;
 
-router.get(
-  "/",
-  asyncWrapper(async (_req: Request, res: Response, _next: NextFunction) => {
+interface HealthCheckResponse {
+  uptime: number;
+  database: boolean;
+  projectsRoute?: boolean;
+}
+
+const checkDatabaseHealth = async (): Promise<boolean> => {
+  try {
     await sequelize.authenticate();
-    res.status(200).json({
-      status: httpStatusText.SUCCESS,
-      message: "Database connection has been established successfully.",
-    });
-  })
-);
+    return true;
+  } catch (error) {
+    console.error("Database health check failed:", error);
+    return false;
+  }
+};
 
-export default router;
+const checkProjectsRouteHealth = async (): Promise<boolean> => {
+  try {
+    const response = await fetch(`${BASE_URL}/projects`);
+    return response.ok;
+  } catch (error) {
+    console.error("`/projects` route health check failed:", error);
+    return false;
+  }
+};
+
+const healthCheck = async (): Promise<HealthCheckResponse> => {
+  const uptime = process.uptime();
+  const isDatabaseHealthy = await checkDatabaseHealth();
+  const isProjectsRouteHealthy = await checkProjectsRouteHealth();
+
+  return {
+    uptime,
+    database: isDatabaseHealthy,
+    projectsRoute: isProjectsRouteHealthy,
+  };
+};
+
+const healthCheckRouter = express.Router();
+
+healthCheckRouter.get("/", async (_, res) => {
+  try {
+    const health = await healthCheck();
+    res.status(200).json(health);
+  } catch (error) {
+    console.error("Health check error:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+export default healthCheckRouter;
