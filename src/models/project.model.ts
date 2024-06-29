b2391f55d89ee6cf87b93311dbc6b22bd0f9c956
Mazen Adel <ma134652134652@gmail.com>
2024-06-29 10:20:32 +0300

    refactor(project.model.ts): change validation logic to the model

--- src/models/project.model.ts
+++ src/models/project.model.ts
@@ -3,11 +3,11 @@ import { v4 as uuidv4 } from "uuid";
 import sequelize from "../config/db";
 
 // Define attributes for the Project model
-interface ProjectAttributes {
+export interface ProjectAttributes {
   id: string;
   project_title: string;
   project_id: string;
-  project_members: string;
+  project_members?: string;
   tasks_manager_url?: string;
   brand_guideline_url?: string;
   ui_ux_url?: string;
@@ -29,7 +29,7 @@ class Project
   public id!: string;
   public project_title!: string;
   public project_id!: string;
-  public project_members!: string;
+  public project_members?: string;
   public tasks_manager_url?: string;
   public brand_guideline_url?: string;
   public ui_ux_url?: string;
@@ -50,15 +50,43 @@ Project.init(
     project_title: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: "project_title cannot be null",
+        },
+        notEmpty: {
+          msg: "project_title cannot be empty",
+        },
+        isString(value: any) {
+          if (typeof value !== "string") {
+            throw new Error("project_title must be a string");
+          }
+        },
+      },
     },
     project_id: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
+      unique: {
+        name: "project_id",
+        msg: "project_id should be unique",
+      },
+      validate: {
+        notNull: {
+          msg: "project_id cannot be null",
+        },
+        notEmpty: {
+          msg: "project_id cannot be empty",
+        },
+        isString(value: any) {
+          if (typeof value !== "string") {
+            throw new Error("project_id must be a string");
+          }
+        },
+      },
     },
     project_members: {
       type: DataTypes.STRING,
-      allowNull: false,
       get() {
         const rawValue = this.getDataValue("project_members");
         return rawValue ? JSON.parse(rawValue) : [];
@@ -66,24 +94,72 @@ Project.init(
       set(value: string[]) {
         this.setDataValue("project_members", JSON.stringify(value));
       },
+      validate: {
+        isArr(value: any) {
+          if (!Array.isArray(JSON.parse(value)))
+            throw new Error("project_members must be an array");
+        },
+      },
     },
     tasks_manager_url: {
       type: DataTypes.TEXT,
+      validate: {
+        isString(value: any) {
+          if (typeof value !== "string") {
+            throw new Error("tasks_manager_url must be a string");
+          }
+        },
+      },
     },
     brand_guideline_url: {
       type: DataTypes.TEXT,
+      validate: {
+        isString(value: any) {
+          if (typeof value !== "string") {
+            throw new Error("brand_guideline_url must be a string");
+          }
+        },
+      },
     },
     ui_ux_url: {
       type: DataTypes.TEXT,
+      validate: {
+        isString(value: any) {
+          if (typeof value !== "string") {
+            throw new Error("ui_ux_url must be a string");
+          }
+        },
+      },
     },
     uml_url: {
       type: DataTypes.TEXT,
+      validate: {
+        isString(value: any) {
+          if (typeof value !== "string") {
+            throw new Error("uml_url must be a string");
+          }
+        },
+      },
     },
     fe_repo_url: {
       type: DataTypes.TEXT,
+      validate: {
+        isString(value: any) {
+          if (typeof value !== "string") {
+            throw new Error("fe_repo_url must be a string");
+          }
+        },
+      },
     },
     be_repo_url: {
       type: DataTypes.TEXT,
+      validate: {
+        isString(value: any) {
+          if (typeof value !== "string") {
+            throw new Error("be_repo_url must be a string");
+          }
+        },
+      },
     },
     last_updated: {
       type: DataTypes.DATE,
@@ -92,7 +168,7 @@ Project.init(
   },
   {
     tableName: "project",
-    sequelize, // pass the sequelize instance
+    sequelize,
     timestamps: false,
   }
 );
